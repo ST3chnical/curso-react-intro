03273469fa09739a8dd16197d892430ec9ad959e
ST3chnical <samuelmercado7b@gmail.com>
2023-09-05 11:20:19 -0500

    Added props and attributes to components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
+import React from "react";
 import {TodoCounter} from "./TodoCounter";
 import {TodoSearch} from "./TodoSearch";
 import {TodoList} from "./TodoList";
 import {TodoItem} from "./TodoItem";
+import {CreateTodoButton} from "./CreateTodoButton";
 import './App.css';
 
+const defaultTodos = [
+    {text: 'Cortar cebolla', completed: true},
+    {text: 'Cortar tomate', completed: true},
+    {text: 'Cortar limon', completed: false},
+    {text: 'Cortar naranja', completed: true}
+];
 
 function App() {
-  return (
-    <div className="App">
-
-        <TodoCounter/>
-        <TodoSearch/>
-
-        <TodoList>
-            <TodoItem/>
-            <TodoItem/>
-            <TodoItem/>
-        </TodoList>
-
-        {/*<CreateTodoButton/>*/}
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <TodoCounter completed={16} total={25}/>
+            <TodoSearch/>
+            <TodoList>
+                {defaultTodos.map(todo => (
+                    <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+                ))}
+            </TodoList>
+            <CreateTodoButton/>
+        </React.Fragment>
+    );
 }
 
 export default App;
