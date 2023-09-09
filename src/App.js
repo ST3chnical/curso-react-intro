14f3b32ed3e39cc0c4ef2e36665de5e0011e8a76
ST3chnical <samuelmercado7b@gmail.com>
2023-09-08 23:19:54 -0500

    Added todo search

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ function App() {
     const completedTodos = todos.filter(todo => !!todo.completed).length;
     const totalTodos = todos.length;
 
+    const searchedTodos = todos.filter(
+        (todo) => {
+            return todo.text.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())
+        }
+    );
+
     return (
         <React.Fragment>
             <TodoCounter completed={completedTodos} total={totalTodos}/>
@@ -28,7 +34,7 @@ function App() {
                 setSearchValue = {setSearchValue}
             />
             <TodoList>
-                {defaultTodos.map(todo => (
+                {searchedTodos.map(todo => (
                     <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
                 ))}
             </TodoList>
